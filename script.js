17db3e81775d9e90607b6058bc44db538420c0e3
Tigro <easterlingme95@gmail.com>
2022-10-20 17:07:33 -0500

    variables declared in JS

---
+++ script.js
@@ -0,0 +1,64 @@
+var time = $("#currentDay");
+var monday = [
+  {
+    id: "0",
+    hour: "9",
+    time: "9",
+    meridiem: "am",
+  },
+  {
+    id: "1",
+    hour: "10",
+    time: "10",
+    meridiem: "am",
+  },
+  {
+    id: "3",
+    hour: "11",
+    time: "11",
+    meridiem: "pm",
+  },
+  {
+    id: "4",
+    hour: "12",
+    time: "12",
+    meridiem: "pm",
+  },
+  {
+    id: "5",
+    hour: "1",
+    time: "13",
+    meridiem: "pm",
+  },
+  {
+    id: "6",
+    hour: "2",
+    time: "14",
+    meridiem: "pm",
+  },
+  {
+    id: "7",
+    hour: "3",
+    time: "15",
+    meridiem: "pm",
+  },
+  {
+    id: "8",
+    hour: "4",
+    time: "16",
+    meridiem: "pm",
+  },
+  {
+    id: "9",
+    hour: "10",
+    time: "22",
+    meridiem: "pm",
+  },
+  {
+    id: "10",
+    hour: "11",
+    time: "23",
+    meridiem: "pm",
+  },
+];
+var containerEl = $(".container");
